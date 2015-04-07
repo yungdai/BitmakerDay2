1466e05e39c510a3854656fad53ae42851129af4
Yung Dai <yungchidai@gmail.com>
2015-04-07 10:29:52 -0400

    Initial Commit

---
+++ BitmakerDay2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BitmakerDay2
+//
+//  Created by Yung Dai on 2015-04-07.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
